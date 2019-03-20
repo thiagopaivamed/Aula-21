64c822606976482094520b2abc90c7962dcae2db
Thiago Paiva  Medeiros <thiagopaivamed@gmail.com>
2019-03-19 21:20:30 -0300

    View Components

---
+++ FaixaEtariaViewComponents/FaixaEtariaViewComponents/Models/PessoaContexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaixaEtariaViewComponents.Models
+{
+    public class PessoaContexto : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        public PessoaContexto(DbContextOptions<PessoaContexto> opcoes) : base(opcoes) { }
+    }
+}
---
+++ FaixaEtariaViewComponents/FaixaEtariaViewComponents/ViewComponents/AdultosViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using FaixaEtariaViewComponents.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaixaEtariaViewComponents.ViewComponents
+{
+    public class AdultosViewComponent : ViewComponent
+    {
+        private readonly PessoaContexto _pessoaContexto;
+
+        public AdultosViewComponent(PessoaContexto pessoaContexto)
+        {
+            _pessoaContexto = pessoaContexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _pessoaContexto.Pessoas.Where(x => x.Idade >= 18 && x.Idade < 60).ToListAsync());
+        }
+    }
+}
